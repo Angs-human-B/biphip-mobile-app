3648e22dcc3bcdd2ac0975a92fe98ecef1391aa6
wahidmurad1 <genie.murad@gmail.com>
2023-10-12 13:11:01 +0600

    gallery video border and border radius add on bottom

--- lib/views/profile/profile_widgets/gallery_videos.dart
+++ lib/views/profile/profile_widgets/gallery_videos.dart
@@ -92,14 +92,13 @@ class CommonGalleryVideoContainer extends StatelessWidget {
               children: [
                 Row(children: [
                   ClipRRect(
-                    borderRadius: BorderRadius.circular(k8BorderRadius),
+                    borderRadius: const BorderRadius.only(topLeft: Radius.circular(k8BorderRadius), topRight: Radius.circular(k8BorderRadius)),
                     child: SizedBox(
-                      height: 101,
                       width: (width - 50) / 2,
                       child: Row(
                         children: [
                           SizedBox(
-                            height: 101,
+                            height: 92,
                             width: (video2 == null && video3 == null) ? (width - 52) / 2 : (width - 52) / 4,
                             child: Image.network(
                               video1,
@@ -117,7 +116,7 @@ class CommonGalleryVideoContainer extends StatelessWidget {
                             children: [
                               video2 != null
                                   ? SizedBox(
-                                      height: video3 == null ? 100 : 50,
+                                      height: video3 == null ? 92 : 45.5,
                                       width: (width - 52) / 4,
                                       child: Image.network(
                                         video2!,
@@ -134,7 +133,7 @@ class CommonGalleryVideoContainer extends StatelessWidget {
                                   : const SizedBox(),
                               video3 != null
                                   ? SizedBox(
-                                      height: video2 == null ? 100 : 50,
+                                      height: video2 == null ? 92 : 45.5,
                                       width: (width - 52) / 4,
                                       child: Image.network(
                                         video3!,
@@ -151,28 +150,62 @@ class CommonGalleryVideoContainer extends StatelessWidget {
                     ),
                   ),
                 ]),
-                kH4sizedBox,
-                SizedBox(
-                  width: (width - 52) / 2,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Container(
+                  width: (width - 50) / 2,
+                  decoration: BoxDecoration(
+                    color: cWhiteColor,
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(5),
+                      bottomRight: Radius.circular(5),
+                    ),
+                    border: Border.all(width: 1, color: cLineColor),
+                    // border: Border(
+                    //   left: BorderSide(
+                    //   width: 1,
+                    //   color: cLineColor,
+                    // ),
+                    //   right: BorderSide(
+                    //   width: 1,
+                    //   color: cLineColor,
+                    // ),
+                    //   bottom:BorderSide(
+                    //   width: 1,
+                    //   color: cLineColor,
+                    // ),
+                    // ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        title,
-                        style: semiBold14TextStyle(cBlackColor),
+                      kH8sizedBox,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: k4Padding),
+                            child: Text(
+                              title,
+                              style: semiBold14TextStyle(cBlackColor),
+                            ),
+                          ),
+                          const Icon(
+                            BipHip.system,
+                            size: kIconSize14,
+                            color: cIconColor,
+                          ),
+                        ],
                       ),
-                      const Icon(
-                        BipHip.system,
-                        size: kIconSize14,
-                        color: cIconColor,
+                      kH4sizedBox,
+                      Padding(
+                        padding: const EdgeInsets.only(left: k4Padding),
+                        child: Text(
+                          subTitle,
+                          style: regular12TextStyle(cSmallBodyTextColor),
+                        ),
                       ),
+                      kH4sizedBox,
                     ],
                   ),
-                ),
-                kH4sizedBox,
-                Text(
-                  subTitle,
-                  style: regular12TextStyle(cSmallBodyTextColor),
                 ),
               ],
             ),
