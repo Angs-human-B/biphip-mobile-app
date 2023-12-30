cc51a948223d23bbce2c3a7ea1cea93747e1663c
sharkeromi <genie.sharkeromi@gmail.com>
2023-12-30 18:38:30 +0600

    Profile section friend family shimmer added

--- lib/views/menu/profile/widgets/profile_friends_gridview.dart
+++ lib/views/menu/profile/widgets/profile_friends_gridview.dart
@@ -20,6 +20,7 @@ class FriendFamilyTab extends StatelessWidget {
             header: ksFriends.tr,
             count: friendController.friendList.length.toString(),
             friendList: friendController.friendList,
+            loading: friendController.isFriendListLoading,
           ),
         ),
         kH12sizedBox,
@@ -30,6 +31,7 @@ class FriendFamilyTab extends StatelessWidget {
               header: ksFamily.tr,
               count: familyController.familyList.length.toString(),
               friendList: familyController.familyList,
+              loading: familyController.isFamilyListLoading,
             ),
           ),
       ],
@@ -38,58 +40,72 @@ class FriendFamilyTab extends StatelessWidget {
 }
 
 class FriendsFamilyGridView extends StatelessWidget {
-  const FriendsFamilyGridView({super.key, required this.header, required this.count, this.seeAll, required this.friendList});
+  const FriendsFamilyGridView({super.key, required this.header, required this.count, this.seeAll, required this.friendList, required this.loading});
 
   final String header;
   final String count;
   final VoidCallback? seeAll;
   final List<FriendFamilyUserData> friendList;
+  final RxBool loading;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
-      child: Column(
-        children: [
-          kH12sizedBox,
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
+      child: Obx(() => Column(
             children: [
-              Text(
-                header,
-                style: semiBold18TextStyle(cBlackColor),
+              kH12sizedBox,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  loading.value
+                      ? ShimmerCommon(
+                          widget: Container(
+                            decoration: BoxDecoration(color: cWhiteColor, borderRadius: k8CircularBorderRadius),
+                            height: 18,
+                            width: 100,
+                          ),
+                        )
+                      : RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: header,
+                                style: semiBold18TextStyle(cBlackColor),
+                              ),
+                              TextSpan(
+                                text: '   $count ${header.toLowerCase()}',
+                                style: regular12TextStyle(cSmallBodyTextColor),
+                              )
+                            ],
+                          ),
+                        ),
+                  CustomTextButton(onPressed: seeAll, text: ksSeeAll.tr, textStyle: semiBold14TextStyle(cPrimaryColor)),
+                ],
               ),
-              kW8sizedBox,
-              Text(
-                '$count $header',
-                style: regular12TextStyle(cSmallBodyTextColor),
-              ),
-              const Spacer(),
-              CustomTextButton(onPressed: seeAll, text: ksSeeAll.tr, textStyle: semiBold14TextStyle(cPrimaryColor)),
+              kH12sizedBox,
+              loading.value
+                  ? const FriendFamilyGridViewShimmer()
+                  : GridView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: friendList.length,
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        childAspectRatio: .8,
+                        crossAxisCount: 3,
+                        crossAxisSpacing: k10Padding,
+                        mainAxisSpacing: k4Padding,
+                      ),
+                      itemBuilder: (BuildContext context, int index) {
+                        var item = friendList[index];
+                        return CustomGridViewContainer(
+                          item: item,
+                        );
+                      },
+                    ),
             ],
-          ),
-          kH12sizedBox,
-          GridView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: friendList.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              childAspectRatio: .8,
-              crossAxisCount: 3,
-              crossAxisSpacing: k10Padding,
-              mainAxisSpacing: k4Padding,
-            ),
-            itemBuilder: (BuildContext context, int index) {
-              var item = friendList[index];
-              return CustomGridViewContainer(
-                item: item,
-              );
-            },
-          ),
-        ],
-      ),
+          )),
     );
   }
 }
@@ -108,22 +124,23 @@ class CustomGridViewContainer extends StatelessWidget {
         children: [
           ClipRRect(
             borderRadius: k8CircularBorderRadius,
-            child: Image.network(
-              item.profilePicture!,
+            child: Container(
+              color: cBlackColor,
               height: 100,
               width: ((width - 72) / 3),
-              filterQuality: FilterQuality.high,
-              fit: BoxFit.fill,
-              errorBuilder: (context, error, stackTrace) => Container(
-                width: width,
-                height: 150,
-                color: cBlackColor,
-                child: const CommonImageErrorBuilder(
-                  icon: BipHip.user,
-                  iconSize: kIconSize120,
+              child: Image.network(
+                item.profilePicture!,
+                filterQuality: FilterQuality.high,
+                fit: BoxFit.fill,
+                errorBuilder: (context, error, stackTrace) => Container(
+                  color: cBlackColor,
+                  child: const CommonImageErrorBuilder(
+                    icon: BipHip.user,
+                    iconSize: kIconSize60,
+                  ),
                 ),
+                loadingBuilder: imageLoadingBuilderCover,
               ),
-              loadingBuilder: imageLoadingBuilderCover,
             ),
           ),
           kH4sizedBox,
@@ -135,6 +152,50 @@ class CustomGridViewContainer extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class FriendFamilyGridViewShimmer extends StatelessWidget {
+  const FriendFamilyGridViewShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: 6,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        childAspectRatio: .8,
+        crossAxisCount: 3,
+        crossAxisSpacing: k10Padding,
+        mainAxisSpacing: k4Padding,
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        return SizedBox(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ShimmerCommon(
+                widget: Container(
+                  decoration: BoxDecoration(color: cWhiteColor, borderRadius: k8CircularBorderRadius),
+                  height: 100,
+                  width: (width - 72) / 3,
+                ),
+              ),
+              kH4sizedBox,
+              ShimmerCommon(
+                widget: Container(
+                  decoration: BoxDecoration(color: cWhiteColor, borderRadius: k8CircularBorderRadius),
+                  height: 16,
+                  width: 60,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
