116acb6479737029fdf90572850ab8b86a709c08
sharkeromi <genie.sharkeromi@gmail.com>
2023-12-30 13:25:14 +0600

    Friend model change

--- lib/models/common/common_friend_family_user_model.dart
+++ lib/models/common/common_friend_family_user_model.dart
@@ -1,31 +1,43 @@
 class FriendFamilyUserData {
   int? id;
+  String? image;
   String? userName;
+  String? referralCode;
   String? firstName;
   String? lastName;
   String? email;
   dynamic phone;
   String? gender;
   DateTime? dob;
-  List<dynamic> profession;
-  List<dynamic> interest;
-  dynamic bio;
-  String? languages;
+  List<String> profession;
+  List<String> interest;
+  String? bio;
+  List<String> languages;
   String? status;
+  dynamic blockTill;
+  String? otp;
   dynamic refId;
   String? relation;
   dynamic relationWithName;
   dynamic relationWithId;
+  String? cover;
+  dynamic badge;
+  dynamic socialProvider;
+  String? referralUrl;
   String? fullName;
-  dynamic profilePicture;
+  String? profilePicture;
   String? coverPhoto;
+  String? currentBadge;
   int? friendStatus;
   int? followStatus;
   String? familyRelationStatus;
+  int? mutualFriend;
 
   FriendFamilyUserData({
     required this.id,
+    required this.image,
     required this.userName,
+    required this.referralCode,
     required this.firstName,
     required this.lastName,
     required this.email,
@@ -37,41 +49,59 @@ class FriendFamilyUserData {
     required this.bio,
     required this.languages,
     required this.status,
+    required this.blockTill,
+    required this.otp,
     required this.refId,
     required this.relation,
     required this.relationWithName,
     required this.relationWithId,
+    required this.cover,
+    required this.badge,
+    required this.socialProvider,
+    required this.referralUrl,
     required this.fullName,
     required this.profilePicture,
     required this.coverPhoto,
+    required this.currentBadge,
     required this.friendStatus,
     required this.followStatus,
     required this.familyRelationStatus,
+    required this.mutualFriend,
   });
 
   factory FriendFamilyUserData.fromJson(Map<String, dynamic> json) => FriendFamilyUserData(
         id: json["id"],
+        image: json["image"],
         userName: json["user_name"],
+        referralCode: json["referral_code"],
         firstName: json["first_name"],
         lastName: json["last_name"],
         email: json["email"],
         phone: json["phone"],
         gender: json["gender"],
         dob: DateTime.parse(json["dob"]),
-        profession: List<dynamic>.from(json["profession"].map((x) => x)),
-        interest: List<dynamic>.from(json["interest"].map((x) => x)),
+        profession: List<String>.from(json["profession"].map((x) => x)),
+        interest: List<String>.from(json["interest"].map((x) => x)),
         bio: json["bio"],
-        languages: json["languages"],
+        languages: List<String>.from(json["languages"].map((x) => x)),
         status: json["status"],
+        blockTill: json["block_till"],
+        otp: json["otp"],
         refId: json["ref_id"],
         relation: json["relation"],
         relationWithName: json["relation_with_name"],
         relationWithId: json["relation_with_id"],
+        cover: json["cover"],
+        badge: json["badge"],
+        socialProvider: json["social_provider"],
+        referralUrl: json["referral_url"],
         fullName: json["full_name"],
         profilePicture: json["profile_picture"],
         coverPhoto: json["cover_photo"],
+        currentBadge: json["current_badge"],
         friendStatus: json["friend_status"],
         followStatus: json["follow_status"],
         familyRelationStatus: json["family_relation_status"],
+        mutualFriend: json["mutual_friend"],
       );
 }
