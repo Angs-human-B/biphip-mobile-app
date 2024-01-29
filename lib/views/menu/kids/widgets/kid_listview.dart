482c1a3d914e90f879d3df2e947f7cdbae156656
wahidmurad1 <genie.murad@gmail.com>
2024-01-29 19:11:58 +0600

    kids listview unnecessary code remove

--- lib/views/menu/kids/widgets/kid_listview.dart
+++ lib/views/menu/kids/widgets/kid_listview.dart
@@ -19,78 +19,73 @@ class KidsListView extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        ListView.builder(
+                        ListView.separated(
+                          separatorBuilder: (context, index) => kH12sizedBox,
                           itemCount: kidsController.kidList.length,
                           shrinkWrap: true,
                           physics: const NeverScrollableScrollPhysics(),
                           itemBuilder: (context, index) {
-                            return Padding(
-                              padding: const EdgeInsets.only(bottom: k12Padding),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(k8BorderRadius),
-                                child: CustomListTile(
-                                  padding: const EdgeInsets.symmetric(horizontal: k8Padding, vertical: k8Padding).copyWith(right: k0Padding),
-                                  borderColor: cLineColor,
-                                  leading: Container(
-                                    height: h40,
-                                    width: h40,
-                                    decoration: const BoxDecoration(
-                                      color: cWhiteColor,
-                                      shape: BoxShape.circle,
-                                    ),
-                                    child: ClipOval(
-                                      child: Image.network(
-                                        kidsController.kidList[index].profilePicture.toString(),
-                                        fit: BoxFit.cover,
-                                        errorBuilder: (context, error, stackTrace) {
-                                          return Image.asset(kiProfileDefaultImageUrl);
-                                        },
-                                        loadingBuilder: imageLoadingBuilder,
-                                      ),
-                                    ),
-                                  ),
-                                  title: Text(
-                                    kidsController.kidList[index].name ?? ksNA.tr,
-                                    style: semiBold16TextStyle(cBlackColor),
-                                  ),
-                                  // trailing: CustomIconButton(
-                                  //     onPress: () {
-                                  //       kidsController.kidId.value = kidsController.kidList[index].id!;
-                                  //       globalController.commonBottomSheet(
-                                  //         context: context,
-                                  //         isScrollControlled: true,
-                                  //         content: EditDeleteContent(),
-                                  //         onPressCloseButton: () {
-                                  //           Get.back();
-                                  //         },
-                                  //         onPressRightButton: null,
-                                  //         rightText: ksDone.tr,
-                                  //         rightTextStyle: semiBold16TextStyle(cPrimaryColor),
-                                  //         title: ksAction.tr,
-                                  //         isRightButtonShow: false,
-                                  //         bottomSheetHeight: 150,
-                                  //       );
-                                  //     },
-                                  //     icon: BipHip.system),
-
-                                  subtitle: Row(
-                                    children: [
-                                      Container(
-                                        width: 8,
-                                        height: 8,
-                                        decoration: const BoxDecoration(
-                                          color: cPrimaryColor,
-                                          shape: BoxShape.circle,
-                                        ),
-                                      ),
-                                      kW4sizedBox,
-                                      Text(
-                                        '2 Notifications',
-                                        style: regular14TextStyle(cBlackColor),
-                                      ),
-                                    ],
+                            return CustomListTile(
+                              padding: const EdgeInsets.only(left: k8Padding, top: k8Padding, bottom: k8Padding),
+                              borderColor: cLineColor,
+                              leading: Container(
+                                height: h40,
+                                width: h40,
+                                decoration: const BoxDecoration(
+                                  color: cWhiteColor,
+                                  shape: BoxShape.circle,
+                                ),
+                                child: ClipOval(
+                                  child: Image.network(
+                                    kidsController.kidList[index].profilePicture.toString(),
+                                    fit: BoxFit.cover,
+                                    errorBuilder: (context, error, stackTrace) {
+                                      return Image.asset(kiProfileDefaultImageUrl);
+                                    },
+                                    loadingBuilder: imageLoadingBuilder,
                                   ),
                                 ),
+                              ),
+                              title: Text(
+                                kidsController.kidList[index].name ?? ksNA.tr,
+                                style: semiBold16TextStyle(cBlackColor),
+                              ),
+                              // trailing: CustomIconButton(
+                              //     onPress: () {
+                              //       kidsController.kidId.value = kidsController.kidList[index].id!;
+                              //       globalController.commonBottomSheet(
+                              //         context: context,
+                              //         isScrollControlled: true,
+                              //         content: EditDeleteContent(),
+                              //         onPressCloseButton: () {
+                              //           Get.back();
+                              //         },
+                              //         onPressRightButton: null,
+                              //         rightText: ksDone.tr,
+                              //         rightTextStyle: semiBold16TextStyle(cPrimaryColor),
+                              //         title: ksAction.tr,
+                              //         isRightButtonShow: false,
+                              //         bottomSheetHeight: 150,
+                              //       );
+                              //     },
+                              //     icon: BipHip.system),
+
+                              subtitle: Row(
+                                children: [
+                                  Container(
+                                    width: 8,
+                                    height: 8,
+                                    decoration: const BoxDecoration(
+                                      color: cPrimaryColor,
+                                      shape: BoxShape.circle,
+                                    ),
+                                  ),
+                                  kW4sizedBox,
+                                  Text(
+                                    '2 Notifications',
+                                    style: regular14TextStyle(cBlackColor),
+                                  ),
+                                ],
                               ),
                             );
                           },
