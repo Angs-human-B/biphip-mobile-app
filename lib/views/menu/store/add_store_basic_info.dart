5aa9a08bec46172c2d4cdc0e92f51ea67b8c3fb2
wahidmurad1 <genie.murad@gmail.com>
2024-01-30 16:20:50 +0600

    add store basic info page design start

---
+++ lib/views/menu/store/add_store_basic_info.dart
@@ -0,0 +1,19 @@
+import 'package:bip_hip/utils/constants/imports.dart';
+
+class AddStoreBasicInfo extends StatelessWidget {
+  const AddStoreBasicInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: cWhiteColor,
+      child: SafeArea(
+        top: false,
+        child: Scaffold(
+          backgroundColor: cWhiteColor,
+          
+        ),
+      ),
+    );
+  }
+}
