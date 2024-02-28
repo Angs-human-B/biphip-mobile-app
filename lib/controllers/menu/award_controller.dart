a825d60c5fd8ddf21cdcb4557e72a112a6eb68ba
wahidmurad1 <genie.murad@gmail.com>
2024-02-28 17:08:05 +0600

    award page text field add

--- lib/controllers/menu/award_controller.dart
+++ lib/controllers/menu/award_controller.dart
@@ -2,6 +2,7 @@
 import 'package:bip_hip/utils/constants/imports.dart';
 
 class AwardController extends GetxController{
-  
+  final TextEditingController awardTextEditingController = TextEditingController();
+  final RxBool isAwardSuffixVisible = RxBool(false);
   
 }
--- lib/views/menu/awards/awards_page.dart
+++ lib/views/menu/awards/awards_page.dart
@@ -1,7 +1,9 @@
+import 'package:bip_hip/controllers/menu/award_controller.dart';
 import 'package:bip_hip/utils/constants/imports.dart';
 
 class AwardsPage extends StatelessWidget {
-  const AwardsPage({super.key});
+  AwardsPage({super.key});
+  final AwardController awardController = Get.find<AwardController>();
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +23,57 @@ class AwardsPage extends StatelessWidget {
               onBack: () {
                 Get.back();
               },
+            ),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+            child: Column(
+              children: [
+                CustomModifiedTextField(
+                    borderRadius: h8,
+                    controller: awardController.awardTextEditingController,
+                    prefixIcon: BipHip.search,
+                    suffixIcon: awardController.isAwardSuffixVisible.value ? BipHip.circleCrossNew : null,
+                    hint: ksSearch.tr,
+                    contentPadding: const EdgeInsets.symmetric(vertical: k12Padding),
+                    textInputStyle: regular16TextStyle(cBlackColor),
+                    onSuffixPress: () {
+                      awardController.isAwardSuffixVisible.value = false;
+                      awardController.awardTextEditingController.clear();
+                    },
+                    onSubmit: (v) {
+                      unfocus(context);
+                      awardController.isAwardSuffixVisible.value = true;
+                    },
+                    onChanged: (v) {
+                      if (awardController.awardTextEditingController.text.toString().trim() != '') {
+                        awardController.isAwardSuffixVisible.value = true;
+                      } else {
+                        awardController.isAwardSuffixVisible.value = false;
+                      }
+                    }),
+                    kH20sizedBox,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      ksYouWon.tr,
+                      style: semiBold18TextStyle(cBlackColor),
+                    ),
+                    CustomElevatedButton(
+                      prefixIcon: BipHip.calendarFill,
+                      prefixIconColor: cIconColor,
+                      buttonWidth: 100,
+                      buttonHeight: h28,
+                      label: ksSelectDate.tr,
+                      textStyle: regular14TextStyle(cSmallBodyTextColor),
+                      onPressed: () {},
+                      buttonColor: cWhiteColor,
+                      borderColor: cLineColor,
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
