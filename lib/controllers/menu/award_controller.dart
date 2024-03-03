d44ae643d71eeb2fb80d2160fc8fbced24d52152
wahidmurad1 <genie.murad@gmail.com>
2024-03-03 12:18:02 +0600

    award first page design done

--- lib/controllers/menu/award_controller.dart
+++ lib/controllers/menu/award_controller.dart
@@ -1,13 +1,77 @@
-
 import 'package:bip_hip/utils/constants/imports.dart';
 
-class AwardController extends GetxController{
+class AwardController extends GetxController {
   final TextEditingController awardTextEditingController = TextEditingController();
   final RxBool isAwardSuffixVisible = RxBool(false);
-   final RxInt selectedCatagoriesIndex = RxInt(0);
+  final RxInt selectedCatagoriesIndex = RxInt(0);
   final RxBool isCategoriesSelected = RxBool(false);
-  void resetAwardData(){
-    selectedCatagoriesIndex.value =0;
-    isCategoriesSelected.value=false;
+  List youWonAwardList = [
+    {
+      "image": "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155475/user-clipart-md.png",
+      "ranking": "10",
+      "WinningDate": "Wining Date: 14 Sep, 2023"
+    },
+    {
+      "image": "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155475/user-clipart-md.png",
+      "ranking": "10",
+      "WinningDate": "Wining Date: 15 Sep, 2023"
+    },
+    {
+      "image": "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155475/user-clipart-md.png",
+      "ranking": "10",
+      "WinningDate": "Wining Date: 16 Sep, 2023"
+    },
+    {
+      "image": "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155475/user-clipart-md.png",
+      "ranking": "10",
+      "WinningDate": "Wining Date: 17 Sep, 2023"
+    },
+    {
+      "image": "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155475/user-clipart-md.png",
+      "ranking": "10",
+      "WinningDate": "Wining Date: 18 Sep, 2023"
+    },
+  ];
+  List othersWinnerAwardList = [
+    {
+      "image": "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155475/user-clipart-md.png",
+      "ranking": "10",
+      "WinningDate": "Wining Date: 19 Sep, 2023"
+    },
+    {
+      "image": "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155475/user-clipart-md.png",
+      "ranking": "10",
+      "WinningDate": "Wining Date: 20 Sep, 2023"
+    },
+    {
+      "image": "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155475/user-clipart-md.png",
+      "ranking": "10",
+      "WinningDate": "Wining Date: 21 Sep, 2023"
+    },
+    {
+      "image": "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155475/user-clipart-md.png",
+      "ranking": "10",
+      "WinningDate": "Wining Date: 22 Sep, 2023"
+    },
+    {
+      "image": "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155475/user-clipart-md.png",
+      "ranking": "10",
+      "WinningDate": "Wining Date: 23 Sep, 2023"
+    },
+    {
+      "image": "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155475/user-clipart-md.png",
+      "ranking": "10",
+      "WinningDate": "Wining Date: 24 Sep, 2023"
+    },
+    {
+      "image": "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155475/user-clipart-md.png",
+      "ranking": "10",
+      "WinningDate": "Wining Date: 25 Sep, 2023"
+    },
+  ];
+
+  void resetAwardData() {
+    selectedCatagoriesIndex.value = 0;
+    isCategoriesSelected.value = false;
   }
 }
--- lib/views/menu/awards/awards_page.dart
+++ lib/views/menu/awards/awards_page.dart
@@ -29,88 +29,141 @@ class AwardsPage extends StatelessWidget {
           ),
           body: Padding(
             padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
-            child: Column(
-              children: [
-                CustomModifiedTextField(
-                    borderRadius: h8,
-                    controller: awardController.awardTextEditingController,
-                    prefixIcon: BipHip.search,
-                    suffixIcon: awardController.isAwardSuffixVisible.value ? BipHip.circleCrossNew : null,
-                    hint: ksSearch.tr,
-                    contentPadding: const EdgeInsets.symmetric(vertical: k12Padding),
-                    textInputStyle: regular16TextStyle(cBlackColor),
-                    onSuffixPress: () {
-                      awardController.isAwardSuffixVisible.value = false;
-                      awardController.awardTextEditingController.clear();
-                    },
-                    onSubmit: (v) {
-                      unfocus(context);
-                      awardController.isAwardSuffixVisible.value = true;
-                    },
-                    onChanged: (v) {
-                      if (awardController.awardTextEditingController.text.toString().trim() != '') {
-                        awardController.isAwardSuffixVisible.value = true;
-                      } else {
+            child: Obx(
+              () => Column(
+                children: [
+                  CustomModifiedTextField(
+                      borderRadius: h8,
+                      controller: awardController.awardTextEditingController,
+                      prefixIcon: BipHip.search,
+                      suffixIcon: awardController.isAwardSuffixVisible.value ? BipHip.circleCrossNew : null,
+                      hint: ksSearch.tr,
+                      contentPadding: const EdgeInsets.symmetric(vertical: k12Padding),
+                      textInputStyle: regular16TextStyle(cBlackColor),
+                      onSuffixPress: () {
                         awardController.isAwardSuffixVisible.value = false;
-                      }
-                    }),
-                kH16sizedBox,
-                Container(
-                  color: cWhiteColor,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        width: width,
-                        height: 50,
-                        child: ListView.builder(
-                          itemCount: interestProfile.length,
-                          shrinkWrap: true,
-                          padding: const EdgeInsets.symmetric(horizontal: k10Padding),
-                          scrollDirection: Axis.horizontal,
-                          itemBuilder: (BuildContext context, i) {
-                            return Obx(
-                              () => Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: k4Padding),
-                                child: CustomChoiceChips(
-                                  label: interestProfile[i],
-                                  isSelected: (awardController.selectedCatagoriesIndex.value == i && awardController.isCategoriesSelected.value),
-                                  onSelected: (value) {
-                                    awardController.selectedCatagoriesIndex.value = i;
-                                    awardController.isCategoriesSelected.value = value;
-                                  },
-                                ),
-                              ),
-                            );
-                          },
+                        awardController.awardTextEditingController.clear();
+                      },
+                      onSubmit: (v) {
+                        unfocus(context);
+                        awardController.isAwardSuffixVisible.value = true;
+                      },
+                      onChanged: (v) {
+                        if (awardController.awardTextEditingController.text.toString().trim() != '') {
+                          awardController.isAwardSuffixVisible.value = true;
+                        } else {
+                          awardController.isAwardSuffixVisible.value = false;
+                        }
+                      }),
+                  kH16sizedBox,
+                  Container(
+                    color: cWhiteColor,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: width,
+                          height: 50,
+                          child: ListView.builder(
+                            itemCount: interestProfile.length,
+                            shrinkWrap: true,
+                            padding: const EdgeInsets.symmetric(horizontal: k10Padding),
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (BuildContext context, i) {
+                              return Obx(() => Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: k4Padding),
+                                    child: CustomChoiceChips(
+                                      label: interestProfile[i],
+                                      isSelected: (awardController.selectedCatagoriesIndex.value == i && awardController.isCategoriesSelected.value),
+                                      onSelected: (value) {
+                                        awardController.selectedCatagoriesIndex.value = i;
+                                        awardController.isCategoriesSelected.value = value;
+                                      },
+                                    ),
+                                  ));
+                            },
+                          ),
                         ),
+                      ],
+                    ),
+                  ),
+                  kH20sizedBox,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        ksYouWon.tr,
+                        style: semiBold18TextStyle(cBlackColor),
+                      ),
+                      CustomElevatedButton(
+                        prefixIcon: BipHip.calendarFill,
+                        prefixIconColor: cIconColor,
+                        buttonWidth: 100,
+                        buttonHeight: h28,
+                        label: ksSelectDate.tr,
+                        textStyle: regular14TextStyle(cSmallBodyTextColor),
+                        onPressed: () {},
+                        buttonColor: cWhiteColor,
+                        borderColor: cLineColor,
                       ),
                     ],
                   ),
-                ),
-                kH20sizedBox,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      ksYouWon.tr,
-                      style: semiBold18TextStyle(cBlackColor),
+                  kH16sizedBox,
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          GridView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: awardController.youWonAwardList.length,
+                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              childAspectRatio: isDeviceScreenLarge() ? 0.9 : 1,
+                              crossAxisCount: 3,
+                              crossAxisSpacing: k16Padding,
+                              mainAxisSpacing: k16Padding,
+                            ),
+                            itemBuilder: (BuildContext context, int index) {
+                              return AwardView(
+                                image: awardController.youWonAwardList[index]['image'],
+                                ranking: awardController.youWonAwardList[index]['ranking'],
+                                winningDate: awardController.youWonAwardList[index]['WinningDate'],
+                              );
+                            },
+                          ),
+                          kH16sizedBox,
+                          Text(
+                            ksOthersWinner.tr,
+                            style: semiBold18TextStyle(cBlackColor),
+                          ),
+                          kH16sizedBox,
+                          GridView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: awardController.othersWinnerAwardList.length,
+                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              childAspectRatio: isDeviceScreenLarge() ? 0.9 : 1,
+                              crossAxisCount: 3,
+                              crossAxisSpacing: k16Padding,
+                              mainAxisSpacing: k16Padding,
+                            ),
+                            itemBuilder: (BuildContext context, int index) {
+                              return AwardView(
+                                image: awardController.othersWinnerAwardList[index]['image'],
+                                ranking: awardController.othersWinnerAwardList[index]['ranking'],
+                                winningDate: awardController.othersWinnerAwardList[index]['WinningDate'],
+                              );
+                            },
+                          ),
+                        ],
+                      ),
                     ),
-                    CustomElevatedButton(
-                      prefixIcon: BipHip.calendarFill,
-                      prefixIconColor: cIconColor,
-                      buttonWidth: 100,
-                      buttonHeight: h28,
-                      label: ksSelectDate.tr,
-                      textStyle: regular14TextStyle(cSmallBodyTextColor),
-                      onPressed: () {},
-                      buttonColor: cWhiteColor,
-                      borderColor: cLineColor,
-                    ),
-                  ],
-                ),
-                AwardView(),
-              ],
+                  ),
+
+                  // AwardView(),
+                ],
+              ),
             ),
           ),
         ),
@@ -120,11 +173,13 @@ class AwardsPage extends StatelessWidget {
 }
 
 class AwardView extends StatelessWidget {
-  const AwardView({super.key});
-
+  const AwardView({super.key, this.image, this.ranking, this.winningDate});
+  final String? image;
+  final String? ranking;
+  final String? winningDate;
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: (width - 40) / 3,
       height: 120,
       child: Stack(
@@ -132,7 +187,7 @@ class AwardView extends StatelessWidget {
           ClipRRect(
             borderRadius: BorderRadius.circular(k8BorderRadius),
             child: Image.network(
-              'https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3155475/user-clipart-md.png',
+              image!,
               width: (width - 40) / 3,
               height: 120,
               fit: BoxFit.contain,
@@ -164,7 +219,7 @@ class AwardView extends StatelessWidget {
                     top: 6,
                     left: 6,
                     child: Text(
-                      '10',
+                      ranking ?? "",
                       style: regular12TextStyle(cWhiteColor),
                     )),
               ],
@@ -178,10 +233,18 @@ class AwardView extends StatelessWidget {
           Positioned(
             left: 6,
             bottom: 6,
-            child: Text(
-              'Wining Date: 14 Sep, 2023',
-              style: semiBold12TextStyle(cWhiteColor),
+            child: SizedBox(
+              width: ((width - 40) / 3) / 1.5,
+              child: Text(
+                winningDate ?? "",
+                style: semiBold12TextStyle(cWhiteColor),
+              ),
             ),
+          ),
+          Positioned(
+            bottom: 6,
+            right: 6,
+            child: SvgPicture.asset(kiAward),
           ),
         ],
       ),
