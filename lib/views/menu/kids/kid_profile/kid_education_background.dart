6609a179b3880540338eaab65a57780c943bf1db
wahidmurad1 <genie.murad@gmail.com>
2024-03-20 16:40:14 +0600

    kid education background commented code remove

--- lib/views/menu/kids/kid_profile/kid_education_background.dart
+++ lib/views/menu/kids/kid_profile/kid_education_background.dart
@@ -36,27 +36,12 @@ class KidEducationBackground extends StatelessWidget {
                     prefixText: kidsController.kidSchoolList[i].ended != null ? ksStudiedAt.tr : ksStudiesAt.tr,
                     subtitlePrefixText:
                         kidHelper.schoolSubtitleText(kidsController.kidSchoolList[i].started ?? DateTime.now(), kidsController.kidSchoolList[i].ended),
-                    // subtitlePrefixText: 'Started at and End at',
                     isAddButton: false,
                     suffixOnPressed: () async {
                       kidHelper.editKidSchool(i);
                     },
                   ),
                 ),
-              // for (int i = 0; i < profileController.collegeDataList.length; i++)
-              // Padding(
-              //   padding: const EdgeInsets.only(bottom: k12Padding),
-              //   child: InfoContainer(
-              //     suffixText: checkNullOrStringNull(profileController.collegeDataList[i].school),
-              //     prefixText: profileController.collegeDataList[i].ended != null ? ksStudiedAt.tr : ksStudiesAt.tr,
-              //     subtitlePrefixText:
-              //         editProfileHelper.schoolSubtitleText(profileController.collegeDataList[i].started, profileController.collegeDataList[i].ended),
-              //     isAddButton: false,
-              //     suffixOnPressed: () async {
-              //       editProfileHelper.editCollege(i);
-              //     },
-              //   ),
-              // ),
               kH4sizedBox
             ],
           ),
