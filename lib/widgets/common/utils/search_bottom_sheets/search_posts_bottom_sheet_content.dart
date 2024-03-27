9a7a44e03a5d3c764f7a20196050261a582719c2
wahidmurad1 <genie.murad@gmail.com>
2024-03-27 17:10:20 +0600

    all bottom sheet content in sepate file

---
+++ lib/widgets/common/utils/search_bottom_sheets/search_posts_bottom_sheet_content.dart
@@ -0,0 +1,163 @@
+
+import 'package:bip_hip/controllers/home/all_search_controller.dart';
+import 'package:bip_hip/utils/constants/imports.dart';
+import 'package:bip_hip/widgets/common/utils/search.dart';
+import 'package:bip_hip/widgets/common/utils/search_widgets/date_posted_content.dart';
+import 'package:bip_hip/widgets/common/utils/search_widgets/posts_category_content.dart';
+import 'package:bip_hip/widgets/common/utils/search_widgets/search_posted_by_content.dart';
+
+class SearchPostsBottomSheetContent extends StatelessWidget {
+  SearchPostsBottomSheetContent({super.key});
+  final AllSearchController allSearchController = Get.find<AllSearchController>();
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() => Column(
+          children: [
+            CustomListTile(
+              leading: const IconContainer(icon: Icons.timer), //!Icon must change
+              title: ksRecentPost.tr,
+              trailing: Obx(() => SizedBox(
+                    width: (width - 330) / 2,
+                    child: CustomCheckBox(
+                        value: allSearchController.isRecentPostCheckBoxSelected.value,
+                        label: "",
+                        onChanged: (v) {
+                          allSearchController.isRecentPostCheckBoxSelected.value = !allSearchController.isRecentPostCheckBoxSelected.value;
+                          allSearchController.postsBottomSheetState();
+                        },
+                        textStyle: regular14TextStyle(cBlackColor)),
+                  )),
+            ),
+            CustomListTile(
+              leading: const IconContainer(icon: BipHip.world),
+              title: ksPostedBy.tr,
+              subtitle: allSearchController.selectedPostedBy.value,
+              trailing: allSearchController.selectedPostedBy.value != ""
+                  ? CustomIconButton(
+                      onPress: () {
+                        allSearchController.selectedPostedBy.value = "";
+                        allSearchController.postsBottomSheetState();
+                      },
+                      icon: BipHip.circleCrossNew,
+                      size: kIconSize20,
+                    )
+                  : null,
+              onPressed: () {
+                allSearchController.temporarySelectedPostedBy.value = allSearchController.selectedPostedBy.value;
+                if (allSearchController.temporarySelectedPostedBy.value == '') {
+                  allSearchController.isPostedByBottomSheetState.value = false;
+                } else {
+                  allSearchController.isPostedByBottomSheetState.value = true;
+                }
+                Get.find<GlobalController>().commonBottomSheet(
+                    context: context,
+                    bottomSheetHeight: height * 0.4,
+                    content: SearchPostedByContent(),
+                    onPressCloseButton: () {
+                      Get.back();
+                    },
+                    onPressRightButton: () {
+                      allSearchController.selectedPostedBy.value = allSearchController.temporarySelectedPostedBy.value;
+                      allSearchController.postsBottomSheetState();
+                      Get.back();
+                    },
+                    rightText: ksDone.tr,
+                    rightTextStyle: semiBold14TextStyle(cPrimaryColor),
+                    title: ksPostedBy.tr,
+                    isRightButtonShow: true,
+                    isBottomSheetRightButtonActive: allSearchController.isPostedByBottomSheetState);
+              },
+            ),
+            CustomListTile(
+              leading: const IconContainer(icon: Icons.date_range), //!Icon must change
+              title: ksDatePosted.tr,
+              subtitle: allSearchController.selectedDatePosted.value,
+              trailing: allSearchController.selectedDatePosted.value == ""
+                  ? null
+                  : CustomIconButton(
+                      onPress: () {
+                        allSearchController.selectedDatePosted.value = "";
+                        allSearchController.postsBottomSheetState();
+                      },
+                      icon: BipHip.circleCrossNew,
+                      size: kIconSize20,
+                    ),
+              onPressed: () {
+                allSearchController.temporarySelectedDatePosted.value = allSearchController.selectedDatePosted.value;
+                // kidsController.temporaryKidRelationId.value = kidsController.kidRelationId.value;
+                if (allSearchController.temporarySelectedDatePosted.value == '') {
+                  allSearchController.isDatePostedBottomSheetState.value = false;
+                } else {
+                  allSearchController.isDatePostedBottomSheetState.value = true;
+                }
+                Get.find<GlobalController>().commonBottomSheet(
+                    context: context,
+                    content: DatePostedContent(),
+                    onPressCloseButton: () {
+                      Get.back();
+                    },
+                    onPressRightButton: () {
+                      allSearchController.selectedDatePosted.value = allSearchController.temporarySelectedDatePosted.value;
+                      allSearchController.postsBottomSheetState();
+                      Get.back();
+                    },
+                    rightText: ksDone.tr,
+                    rightTextStyle: semiBold14TextStyle(cPrimaryColor),
+                    title: ksDatePosted.tr,
+                    isRightButtonShow: true,
+                    isBottomSheetRightButtonActive: allSearchController.isDatePostedBottomSheetState);
+              },
+            ),
+            CustomListTile(
+              leading: const IconContainer(icon: BipHip.menuFill),
+              title: ksCategory.tr,
+              subtitle: allSearchController.selectedCategory.value,
+              trailing: allSearchController.selectedCategory.value == ""
+                  ? null
+                  : CustomIconButton(
+                      onPress: () {
+                        allSearchController.selectedCategory.value = "";
+                        allSearchController.postsBottomSheetState();
+                      },
+                      icon: BipHip.circleCrossNew,
+                      size: kIconSize20,
+                    ),
+              onPressed: () {
+                allSearchController.temporarySelectedCategory.value = allSearchController.selectedCategory.value;
+                if (allSearchController.temporarySelectedCategory.value == '') {
+                  allSearchController.isCategoryBottomSheetState.value = false;
+                } else {
+                  allSearchController.isCategoryBottomSheetState.value = true;
+                }
+                Get.find<GlobalController>().commonBottomSheet(
+                    context: context,
+                    content: PostsCategoryContent(),
+                    onPressCloseButton: () {
+                      Get.back();
+                    },
+                    onPressRightButton: () {
+                      allSearchController.selectedCategory.value = allSearchController.temporarySelectedCategory.value;
+                      allSearchController.postsBottomSheetState();
+                      Get.back();
+                    },
+                    rightText: ksDone.tr,
+                    rightTextStyle: semiBold14TextStyle(cPrimaryColor),
+                    title: ksCategory.tr,
+                    isRightButtonShow: true,
+                    isBottomSheetRightButtonActive: allSearchController.isCategoryBottomSheetState);
+              },
+            ),
+            kH24sizedBox,
+            CustomElevatedButton(
+              label: ksShowResult.tr,
+              buttonWidth: width - 40,
+              buttonHeight: h32,
+              onPressed: allSearchController.isPostsBottomSheetResetOrShowResult.value ? () {} : null,
+              buttonColor: cPrimaryColor,
+              textStyle: semiBold14TextStyle(cWhiteColor),
+            ),
+          ],
+        ));
+  }
+}
+
---
+++ lib/widgets/common/utils/search_bottom_sheets/search_sell_posts_bottom_sheet_content.dart
@@ -0,0 +1,188 @@
+
+import 'package:bip_hip/controllers/home/all_search_controller.dart';
+import 'package:bip_hip/utils/constants/imports.dart';
+import 'package:bip_hip/widgets/common/utils/search.dart';
+import 'package:bip_hip/widgets/common/utils/search_widgets/search_posted_by_content.dart';
+import 'package:bip_hip/widgets/common/utils/search_widgets/sell_post_post_type_content.dart';
+import 'package:bip_hip/widgets/common/utils/search_widgets/sell_post_product_condition.dart';
+import 'package:bip_hip/widgets/common/utils/search_widgets/sell_post_product_condition_content.dart';
+
+class SearchSellPostsBottomSheetContent extends StatelessWidget {
+  SearchSellPostsBottomSheetContent({super.key});
+  final AllSearchController allSearchController = Get.find<AllSearchController>();
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() => Column(
+          children: [
+            CustomListTile(
+              leading: const IconContainer(icon: BipHip.world),
+              title: ksPostType.tr,
+              subtitle: allSearchController.selectedSellPostType.value,
+              trailing: allSearchController.selectedSellPostType.value == ""
+                  ? null
+                  : CustomIconButton(
+                      onPress: () {
+                        allSearchController.selectedSellPostType.value = "";
+                        allSearchController.sellPostBottomSheetState();
+                      },
+                      icon: BipHip.circleCrossNew,
+                      size: kIconSize20,
+                    ),
+              onPressed: () {
+                allSearchController.temporarySelectedSellPostType.value = allSearchController.selectedSellPostType.value;
+                if (allSearchController.temporarySelectedSellPostType.value == '') {
+                  allSearchController.isSellPostTypeBottomSheetState.value = false;
+                } else {
+                  allSearchController.isSellPostTypeBottomSheetState.value = true;
+                }
+                Get.find<GlobalController>().commonBottomSheet(
+                    context: context,
+                    bottomSheetHeight: height * 0.25,
+                    content: SellPostPostTypeContent(),
+                    onPressCloseButton: () {
+                      Get.back();
+                    },
+                    onPressRightButton: () {
+                      allSearchController.selectedSellPostType.value = allSearchController.temporarySelectedSellPostType.value;
+                      allSearchController.sellPostBottomSheetState();
+                      Get.back();
+                    },
+                    rightText: ksDone.tr,
+                    rightTextStyle: semiBold14TextStyle(cPrimaryColor),
+                    title: ksPostType.tr,
+                    isRightButtonShow: true,
+                    isBottomSheetRightButtonActive: allSearchController.isSellPostTypeBottomSheetState);
+              },
+            ),
+            CustomListTile(
+              leading: const IconContainer(icon: BipHip.activity), //!Icon must change
+              title: ksCondition.tr,
+              subtitle: allSearchController.selectedSellPostCondition.value,
+              trailing: allSearchController.selectedSellPostCondition.value == ""
+                  ? null
+                  : CustomIconButton(
+                      onPress: () {
+                        allSearchController.selectedSellPostCondition.value = "";
+                        allSearchController.sellPostBottomSheetState();
+                      },
+                      icon: BipHip.circleCrossNew,
+                      size: kIconSize20,
+                    ),
+              onPressed: () {
+                allSearchController.temporarySelectedSellPostCondition.value = allSearchController.selectedSellPostCondition.value;
+                if (allSearchController.temporarySelectedSellPostCondition.value == '') {
+                  allSearchController.isSellPostConditionBottomSheetState.value = false;
+                } else {
+                  allSearchController.isSellPostConditionBottomSheetState.value = true;
+                }
+                Get.find<GlobalController>().commonBottomSheet(
+                    context: context,
+                    content: SellPostProductConditionContent(),
+                    onPressCloseButton: () {
+                      Get.back();
+                    },
+                    onPressRightButton: () {
+                      allSearchController.selectedSellPostCondition.value = allSearchController.temporarySelectedSellPostCondition.value;
+                      allSearchController.sellPostBottomSheetState();
+                      Get.back();
+                    },
+                    rightText: ksDone.tr,
+                    rightTextStyle: semiBold14TextStyle(cPrimaryColor),
+                    title: ksCondition.tr,
+                    isRightButtonShow: true,
+                    isBottomSheetRightButtonActive: allSearchController.isSellPostConditionBottomSheetState);
+              },
+            ),
+            CustomListTile(
+              leading: const IconContainer(icon: BipHip.world),
+              title: ksPostedBy.tr,
+              subtitle: allSearchController.selectedPostedBy.value,
+              trailing: allSearchController.selectedPostedBy.value == ""
+                  ? null
+                  : CustomIconButton(
+                      onPress: () {
+                        allSearchController.selectedPostedBy.value = "";
+                        allSearchController.sellPostBottomSheetState();
+                      },
+                      icon: BipHip.circleCrossNew,
+                      size: kIconSize20,
+                    ),
+              onPressed: () {
+                allSearchController.temporarySelectedPostedBy.value = allSearchController.selectedPostedBy.value;
+                if (allSearchController.temporarySelectedPostedBy.value == "") {
+                  allSearchController.isPostedByBottomSheetState.value = false;
+                } else {
+                  allSearchController.isPostedByBottomSheetState.value = true;
+                }
+                Get.find<GlobalController>().commonBottomSheet(
+                    context: context,
+                    bottomSheetHeight: height * 0.4,
+                    content: SearchPostedByContent(),
+                    onPressCloseButton: () {
+                      Get.back();
+                    },
+                    onPressRightButton: () {
+                      allSearchController.selectedPostedBy.value = allSearchController.temporarySelectedPostedBy.value;
+                      allSearchController.sellPostBottomSheetState();
+                      Get.back();
+                    },
+                    rightText: ksDone.tr,
+                    rightTextStyle: semiBold14TextStyle(cPrimaryColor),
+                    title: ksPostedBy.tr,
+                    isRightButtonShow: true,
+                    isBottomSheetRightButtonActive: allSearchController.isPostedByBottomSheetState);
+              },
+            ),
+            CustomListTile(
+              leading: const IconContainer(icon: BipHip.menuFill),
+              title: ksCategory.tr,
+              subtitle: allSearchController.selectedSellPostProductCategory.value,
+              trailing: allSearchController.selectedSellPostProductCategory.value == ""
+                  ? null
+                  : CustomIconButton(
+                      onPress: () {
+                        allSearchController.selectedSellPostProductCategory.value = "";
+                        allSearchController.sellPostBottomSheetState();
+                      },
+                      icon: BipHip.circleCrossNew,
+                      size: kIconSize20,
+                    ),
+              onPressed: () {
+                allSearchController.temporarySelectedSellPostProductCategory.value = allSearchController.selectedSellPostProductCategory.value;
+                if (allSearchController.temporarySelectedSellPostProductCategory.value == '') {
+                  allSearchController.isSellPostProductConditionBottomSheetState.value = false;
+                } else {
+                  allSearchController.isSellPostProductConditionBottomSheetState.value = true;
+                }
+                Get.find<GlobalController>().commonBottomSheet(
+                    context: context,
+                    content: SellPostProductCategoryContent(),
+                    onPressCloseButton: () {
+                      Get.back();
+                    },
+                    onPressRightButton: () {
+                      allSearchController.selectedSellPostProductCategory.value = allSearchController.temporarySelectedSellPostProductCategory.value;
+                      allSearchController.sellPostBottomSheetState();
+                      Get.back();
+                    },
+                    rightText: ksDone.tr,
+                    rightTextStyle: semiBold14TextStyle(cPrimaryColor),
+                    title: ksCategory.tr,
+                    isRightButtonShow: true,
+                    isBottomSheetRightButtonActive: allSearchController.isSellPostProductConditionBottomSheetState);
+              },
+            ),
+            kH24sizedBox,
+            CustomElevatedButton(
+              label: ksShowResult.tr,
+              buttonWidth: width - 40,
+              buttonHeight: h32,
+              onPressed: allSearchController.isSellPostBottomSheetResetOrShowResult.value ? () {} : null,
+              buttonColor: cPrimaryColor,
+              textStyle: semiBold14TextStyle(cWhiteColor),
+            ),
+          ],
+        ));
+  }
+}
+
