ea8868a23c4ce35a1fea3347b906dda15070a240
sharkeromi <genie.sharkeomi@gmail.com>
2024-04-21 15:42:51 +0600

    Comment conflict issue fix

--- lib/widgets/post/comment_widget.dart
+++ lib/widgets/post/comment_widget.dart
@@ -121,46 +121,46 @@ class CommentWidget extends StatelessWidget {
                       ],
                     ),
                   ),
-
                 ),
               ),
-            ),
-            if (isLink || isImageComment) kH4sizedBox,
-            if (isLink && !isImageComment)
-              Container(
-                width: width - 80,
-                decoration: BoxDecoration(color: cWhiteColor, border: Border.all(color: cLineColor), borderRadius: k8CircularBorderRadius),
-                child: Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: AnyLinkPreview(
-                    link: commentLink ?? '',
-                    removeElevation: true,
-                    backgroundColor: cWhiteColor,
-                    displayDirection: UIDirection.uiDirectionHorizontal,
-                    showMultimedia: true,
-                    bodyMaxLines: 2,
-                    previewHeight: 84,
-                    bodyTextOverflow: TextOverflow.ellipsis,
-                    titleStyle: semiBold14TextStyle(cBlackColor),
-                    bodyStyle: regular12TextStyle(cSmallBodyTextColor),
+
+              if (isLink || isImageComment) kH4sizedBox,
+              if (isLink && !isImageComment)
+                Container(
+                  width: width - 80,
+                  decoration: BoxDecoration(color: cWhiteColor, border: Border.all(color: cLineColor), borderRadius: k8CircularBorderRadius),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: AnyLinkPreview(
+                      link: commentLink ?? '',
+                      removeElevation: true,
+                      backgroundColor: cWhiteColor,
+                      displayDirection: UIDirection.uiDirectionHorizontal,
+                      showMultimedia: true,
+                      bodyMaxLines: 2,
+                      previewHeight: 84,
+                      bodyTextOverflow: TextOverflow.ellipsis,
+                      titleStyle: semiBold14TextStyle(cBlackColor),
+                      bodyStyle: regular12TextStyle(cSmallBodyTextColor),
+                    ),
                   ),
                 ),
-              ),
-            if (!isLink && image != null)
-              SizedBox(
-                width: isDeviceScreenLarge() ? 150 : 120,
-                height: isDeviceScreenLarge() ? 150 : 120,
-                child: ClipRRect(
-                  borderRadius: k8CircularBorderRadius,
-                  child: Image.network(
-                    image!,
-                    fit: BoxFit.cover,
-                    // errorBuilder: (context, error, stackTrace) => const Icon(
-                    //   BipHip.user,
-                    //   size: kIconSize16,
-                    //   color: cIconColor,
-                    // ),
-                    // loadingBuilder: imageLoadingBuilder,
+              if (!isLink && image != null)
+                SizedBox(
+                  width: isDeviceScreenLarge() ? 150 : 120,
+                  height: isDeviceScreenLarge() ? 150 : 120,
+                  child: ClipRRect(
+                    borderRadius: k8CircularBorderRadius,
+                    child: Image.network(
+                      image!,
+                      fit: BoxFit.cover,
+                      // errorBuilder: (context, error, stackTrace) => const Icon(
+                      //   BipHip.user,
+                      //   size: kIconSize16,
+                      //   color: cIconColor,
+                      // ),
+                      // loadingBuilder: imageLoadingBuilder,
+                    ),
                   ),
                 ),
               if (timePassed != null)
@@ -461,111 +461,111 @@ class CommentWidget extends StatelessWidget {
                               ),
                               child: Get.find<GlobalController>().getColoredCommentReaction(selfReaction),
                             ),
+                          ),
 
+                        kW16sizedBox,
+                        if (isReplyButtonShown)
+                          InkWell(
+                            onTap: replyButtonOnPressed,
+                            child: Text(
+                              ksReply.tr,
+                              style: regular10TextStyle(cSmallBodyTextColor),
+                            ),
                           ),
-                        ),
-                      kW16sizedBox,
-                      if (isReplyButtonShown)
-                        InkWell(
-                          onTap: replyButtonOnPressed,
-                          child: Text(
-                            ksReply.tr,
-                            style: regular10TextStyle(cSmallBodyTextColor),
+                        // if (isSendMessageShown)
+                        // InkWell(
+                        //   onTap: sendMessageOnPressed,
+                        //   child: Text(
+                        //     ksSendMessage.tr,
+                        //     style: regular10TextStyle(cSmallBodyTextColor),
+                        //   ),
+                        // ),
+                        kW16sizedBox,
+                        if (isHideButtonShown)
+                          InkWell(
+                            onTap: hideButtonOnPressed,
+                            child: Text(
+                              ksHide.tr,
+                              style: regular10TextStyle(cSmallBodyTextColor),
+                            ),
                           ),
-                        ),
-                      // if (isSendMessageShown)
-                      // InkWell(
-                      //   onTap: sendMessageOnPressed,
-                      //   child: Text(
-                      //     ksSendMessage.tr,
-                      //     style: regular10TextStyle(cSmallBodyTextColor),
-                      //   ),
-                      // ),
-                      kW16sizedBox,
-                      if (isHideButtonShown)
-                        InkWell(
-                          onTap: hideButtonOnPressed,
-                          child: Text(
-                            ksHide.tr,
-                            style: regular10TextStyle(cSmallBodyTextColor),
-                          ),
-                        ),
-                      const Spacer(),
-                      if (isReactButtonShown) const ReactionView(isPost: false, reactCount: null)
-                    ],
+                        const Spacer(),
+                        if (isReactButtonShown) const ReactionView(isPost: false, reactCount: null)
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            kH4sizedBox,
-            // if (replyList == []) Text('View 7 more replies', style: semiBold14TextStyle(cSmallBodyTextColor)),
-            if (replyList != [])
-              // SizedBox(
-              // width: width - 80,
-              // child: ListView.builder(
-              // shrinkWrap: true,
-              // itemCount: replyList.length,
-              // itemBuilder: (context, index) {
-              for (int index = 0; index < replyList.length; index++)
-                // var item = replyList;
-                InkWell(
-                  onTap: () {
-                    Get.find<PostReactionController>().replyId.value = replyList[index].id!;
-                    Get.find<PostReactionController>().selectedReplyIndex.value = index;
-                    Get.find<PostReactionController>().commentId.value = commentId!;
-                    Get.find<GlobalController>().commonBottomSheet(
-                        context: context,
-                        bottomSheetHeight: height * 0.4,
-                        content: ReplyBottomSheetContent(),
-                        onPressCloseButton: () {
-                          Get.back();
-                        },
-                        onPressRightButton: () {},
-                        rightText: "",
-                        rightTextStyle: regular10TextStyle(cWhiteColor),
-                        title: "",
-                        isRightButtonShow: false);
-                  },
-                  child: ReplyCommentWidget(
-                    profileImage: replyList[index].user?.profilePicture ?? "",
-                    timePassed: Get.find<HomeController>().postTimeDifference(replyList[index].createdAt),
-                    isLikeButtonShown: true,
-                    isReplyButtonShown: true,
-                    isReactButtonShown: true,
-                    isLink: false,
-                    reactCount: 2,
-                    userName: replyList[index].user?.fullName ?? ksNA.tr,
-                    isImageComment: replyList[index].image != null ? true : false,
-                    comment: Get.find<PostReactionController>().formatMentions(replyList[index].reply ?? "", context),
-                    commentLink: "",
-                    image: replyList[index].image,
-                  ),
-                  // );
-                  // }),
-                ),
-            kH8sizedBox,
-            if (Get.find<PostReactionController>().commentId.value == commentId && Get.find<PostReactionController>().isReplyTextFieldShow.value)
-              SizedBox(
-                  width: width - 80,
-                  height: 132,
-                  child: ReplyTextField(
-                    onPressedCamera: () async {
-                      await Get.find<GlobalController>().selectImageSource(Get.find<PostReactionController>().isReplyImageChanged,
-                          Get.find<PostReactionController>().replyImageLink, Get.find<PostReactionController>().replyImageFile, 'gallery', false);
-                      Get.find<PostReactionController>().replySendEnabled();
+              kH4sizedBox,
+              // if (replyList == []) Text('View 7 more replies', style: semiBold14TextStyle(cSmallBodyTextColor)),
+              if (replyList != [])
+                // SizedBox(
+                // width: width - 80,
+                // child: ListView.builder(
+                // shrinkWrap: true,
+                // itemCount: replyList.length,
+                // itemBuilder: (context, index) {
+                for (int index = 0; index < replyList.length; index++)
+                  // var item = replyList;
+                  InkWell(
+                    onTap: () {
+                      Get.find<PostReactionController>().replyId.value = replyList[index].id!;
+                      Get.find<PostReactionController>().selectedReplyIndex.value = index;
+                      Get.find<PostReactionController>().commentId.value = commentId!;
+                      Get.find<GlobalController>().commonBottomSheet(
+                          context: context,
+                          bottomSheetHeight: height * 0.4,
+                          content: ReplyBottomSheetContent(),
+                          onPressCloseButton: () {
+                            Get.back();
+                          },
+                          onPressRightButton: () {},
+                          rightText: "",
+                          rightTextStyle: regular10TextStyle(cWhiteColor),
+                          title: "",
+                          isRightButtonShow: false);
                     },
-                    onPressedSend: Get.find<PostReactionController>().isReplySendEnable.value
-                        ? () async {
-                            if (Get.find<PostReactionController>().isUpdateReply.value) {
-                              await Get.find<PostReactionController>().updateReply(context);
-                            } else {
-                              await Get.find<PostReactionController>().postReply(context);
-                              Get.find<PostReactionController>().replyMentionKey.currentState!.controller!.text = "";
+                    child: ReplyCommentWidget(
+                      profileImage: replyList[index].user?.profilePicture ?? "",
+                      timePassed: Get.find<HomeController>().postTimeDifference(replyList[index].createdAt),
+                      isLikeButtonShown: true,
+                      isReplyButtonShown: true,
+                      isReactButtonShown: true,
+                      isLink: false,
+                      reactCount: 2,
+                      userName: replyList[index].user?.fullName ?? ksNA.tr,
+                      isImageComment: replyList[index].image != null ? true : false,
+                      comment: Get.find<PostReactionController>().formatMentions(replyList[index].reply ?? "", context),
+                      commentLink: "",
+                      image: replyList[index].image,
+                    ),
+                    // );
+                    // }),
+                  ),
+              kH8sizedBox,
+              if (Get.find<PostReactionController>().commentId.value == commentId && Get.find<PostReactionController>().isReplyTextFieldShow.value)
+                SizedBox(
+                    width: width - 80,
+                    height: 132,
+                    child: ReplyTextField(
+                      onPressedCamera: () async {
+                        await Get.find<GlobalController>().selectImageSource(Get.find<PostReactionController>().isReplyImageChanged,
+                            Get.find<PostReactionController>().replyImageLink, Get.find<PostReactionController>().replyImageFile, 'gallery', false);
+                        Get.find<PostReactionController>().replySendEnabled();
+                      },
+                      onPressedSend: Get.find<PostReactionController>().isReplySendEnable.value
+                          ? () async {
+                              if (Get.find<PostReactionController>().isUpdateReply.value) {
+                                await Get.find<PostReactionController>().updateReply(context);
+                              } else {
+                                await Get.find<PostReactionController>().postReply(context);
+                                Get.find<PostReactionController>().replyMentionKey.currentState!.controller!.text = "";
+                              }
                             }
-                          }
-                        : null,
-                  )),
-            if (Get.find<PostReactionController>().commentId.value == commentId) kH8sizedBox,
-          ],
+                          : null,
+                    )),
+              if (Get.find<PostReactionController>().commentId.value == commentId) kH8sizedBox,
+            ],
+          ),
         ),
       ],
     );
