39803b8613915cc8069f0732c7a349eaf77d868a
wahidmurad1 <genie.murad@gmail.com>
2024-06-03 18:26:46 +0600

    dashboard performence page top bottom sheet add and Line Chart widget add

--- lib/views/dashboard/dashboard_award/dashboard_performance.dart
+++ lib/views/dashboard/dashboard_award/dashboard_performance.dart
@@ -1,12 +1,19 @@
+import 'package:bip_hip/controllers/dashboard/dashboard_controller.dart';
 import 'package:bip_hip/utils/constants/imports.dart';
+import 'package:bip_hip/views/dashboard/dashboard_award/dashboard_award.dart';
 
 class DashboardPerformance extends StatelessWidget {
-  const DashboardPerformance({super.key});
+  DashboardPerformance({super.key});
+  final DashboardController dashboardController = Get.find<DashboardController>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-       backgroundColor: cWhiteColor,
+    return Container(
+      color: cWhiteColor,
+      child: SafeArea(
+        top: false,
+        child: Scaffold(
+          backgroundColor: cWhiteColor,
           appBar: PreferredSize(
             preferredSize: const Size.fromHeight(kAppBarSize),
             //* info:: appBar
@@ -19,6 +26,75 @@ class DashboardPerformance extends StatelessWidget {
               },
             ),
           ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+            child: Column(
+              children: [
+                kH16sizedBox,
+                InkWell(
+                  onTap: () {
+                    Get.find<GlobalController>().blankBottomSheet(
+                      context: context,
+                      bottomSheetHeight: height * 0.5,
+                      content: QuizTimeFilterBottomSheetContent(),
+                    );
+                  },
+                  child: Row(
+                    children: [
+                      Container(
+                        height: h28,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(k4BorderRadius),
+                          color: cNeutralColor,
+                        ),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: k8Padding, vertical: k4Padding),
+                              child: Text(
+                                dashboardController.selectedQuizTimeRangeValue.value,
+                                style: regular12TextStyle(cBlackColor),
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(top: k4Padding, bottom: k4Padding, right: k8Padding),
+                              child: Icon(
+                                BipHip.downArrow,
+                                size: kIconSize20,
+                                color: cBlackColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                kH20sizedBox,
+                const LineChartWidget(
+                  titleText: "Win",
+                  filterDateTimeText: "From this month",
+                  totalValue: "5",
+                  percentText: "+2%",
+                  progressColor: cGreenColor,
+                  progressBottomColor: cGreenTintColor,
+                  percentTextColor: cGreenColor,
+                ),
+                kH16sizedBox,
+                const LineChartWidget(
+                  titleText: "Winning Ratio",
+                  filterDateTimeText: "From this month",
+                  totalValue: "2",
+                  percentText: "-16%",
+                  progressColor: cPrimaryColor,
+                  progressBottomColor: cPrimaryTint2Color,
+                  percentTextColor: cRedColor,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
