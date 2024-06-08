1588d945653d5d6f2307604c9d004146d0ac0e22
wahidmurad1 <genie.murad@gmail.com>
2024-06-08 17:46:34 +0600

    dashboard donation i donete page design

--- lib/controllers/dashboard/dashboard_controller.dart
+++ lib/controllers/dashboard/dashboard_controller.dart
@@ -4,14 +4,14 @@ class DashboardController extends GetxController {
   final RxList donationTapButtonText = RxList(["I donated", "My applications"]);
   final RxList donationTapButtonState = RxList([true, false]);
   void dashboardIDonatedTapableButtonOnPressed() async {
-    quizToggleType(0);
+    dashboardDonationToggleType(0);
   }
 
   void dashboardMyDonationTapableButtonOnPressed() async {
-    quizToggleType(1);
+    dashboardDonationToggleType(1);
   }
 
-  void quizToggleType(int index) {
+  void dashboardDonationToggleType(int index) {
     switch (index) {
       case 0:
         donationTapButtonState[0] = true;
@@ -23,4 +23,43 @@ class DashboardController extends GetxController {
         break;
     }
   }
+
+  final RxList dashboardDonationPost = RxList([
+    {
+      "productImage": "https://5.imimg.com/data5/XL/EN/MY-70607897/selection_941-500x500.png",
+      "productTitle": "Kawasaki Ninja® ZX™-10R | Supersport Motorcycle",
+      "postDate": "3Feb, 2024",
+      "postCount": "0",
+      "engagementCount": "0",
+      "giftCount": "0",
+      "amount": "\$10"
+    },
+    {
+      "productImage": "https://imgd.aeplcdn.com/642x361/n/cw/ec/126607/honda-cbr500r-right-side-view0.jpeg?isig=0&q=80",
+      "productTitle": "Honda CBR150R Repsol Edition",
+      "postDate": "10Feb, 2024",
+      "postCount": "0",
+      "engagementCount": "0",
+      "giftCount": "0",
+       "amount": "\$20"
+    },
+    {
+      "productImage": "https://www.otocapital.in/_next/image?url=https%3A%2F%2Fassets.otocapital.in%2Fproduction%2Fsports-bike-in-india.png&w=1024&q=75",
+      "productTitle": "Sports bike",
+      "postDate": "3Feb, 2024",
+      "postCount": "0",
+      "engagementCount": "0",
+      "giftCount": "0",
+       "amount": "\$30"
+    },
+    {
+      "productImage": "https://media.e-valy.com/cms/products/images/b71f027e-9793-4dc2-8d26-1111cbba1c81",
+      "productTitle": "Yamaha MT-15 Motor Bike - 155CC - Black",
+      "postDate": "15Feb, 2024",
+      "postCount": "0",
+      "engagementCount": "0",
+      "giftCount": "0",
+       "amount": "\$40"
+    },
+  ]);
 }
